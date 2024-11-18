7fae71082bc97ff363b1bf1d49efd0861d41b106
fbarar <baraf460@newschool.edu>
2024-11-18 09:45:20 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-import pandas as pd
 import streamlit as st
 import numpy as np
 import pickle
-
-df = pd.read_csv("obesity.csv")
-with open("obesity.pkl", "wb") as f:
-    pickle.dump(df, f)
 
 st.title("Obesity Predictor")
 
@@ -14,9 +9,9 @@ gender = st.selectbox(
     ("Male", "Female"),
 )
 
-age = st.slider("How old are you?", 0, 130, 25)
+age = st.slider("How old are you?", 0, 100, 25)
 
-weight = st.slider("How much do you weigh?", 0, 130, 25)
+weight = st.slider("How much do you weigh?", 0, 200, 25)
 
 history = st.radio(
     "Does your family have a history of being overweight?",
@@ -25,7 +20,7 @@ history = st.radio(
 
 obesity_level = np.array([[gender, age, weight, history]])
 
-with open('obesity.pkl', 'wb') as f:
+with open('rf.pkl', 'rb') as f:
     model = pickle.load(f)
 
 prediction = model.predict(obesity_level)
